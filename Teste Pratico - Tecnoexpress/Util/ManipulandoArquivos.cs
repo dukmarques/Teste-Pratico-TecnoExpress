66391d112e870e82aa140c7a37d746d76eb59049
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2020-02-16 14:05:39 -0300

    Classe de manipulação de arquivos

--- Teste Pratico - Tecnoexpress/Util/ManipulandoArquivos.cs
+++ Teste Pratico - Tecnoexpress/Util/ManipulandoArquivos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,43 @@ namespace Util
 {
     public class ManipulandoArquivos
     {
+        public static string readFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                using (Stream sr = File.Open(path, FileMode.Open))
+                {
+                    StreamReader read = new StreamReader(sr);
+                    string linha = read.ReadLine();
+
+                    while (linha != null)
+                    {
+                        /*Código*/
+                    }
+
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        //Método para adicionar contéúdo ao arquivo sem sobrescrever o conteúdo existente
+        public static void writeFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    /*Código*/
+                }
+            }
+            else
+            {
+                /*A definir*/
+            }
+        }
     }
 }
